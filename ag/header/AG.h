3a42e20319cfe175a915d88e067d6c55fe99c0ad
Iury Xavier <86579744+Iury-Xavier@users.noreply.github.com>
2022-05-04 08:36:50 -0300

    Add files via upload

--- ag/header/AG.h
+++ ag/header/AG.h
@@ -3,18 +3,31 @@
 
 #include "Populacao.h"
 #include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace::std;
 
 class AG{
 
     public:
-        static void setParametros(int tam_ind, int tam_pop, int mut_prob, int cross_p_corte){
+        static void setParametros(int tam_ind, int tam_pop, int mut_prob, int cross_p_corte, string path_mochila){
             Individuo::setTamanho(tam_ind);
             Individuo::setProbMutacao(mut_prob);
             Populacao::setTamanho(tam_pop);
             AG::cross_p_corte = cross_p_corte;
             AG::setted = true;
+            AG::path_mochila = path_mochila;
+        }
+
+        static void lerMochila(){
+            File *f = fopen(path_mochila.c_str(), "r");
+            int tam, cap;
+            fscanf(f, "%d", &tam);
+            fscanf(f, "%d", &cap);
+
+            f.close();
         }
 
         AG();
@@ -27,6 +40,10 @@ class AG{
     private:
         static int cross_p_corte;
         static bool setted;
+
+        static vector<int> dados_peso;
+        static vector<int> dados_beneficio;
+        static string path_mochila;
 
         void inicializar();
         void crossover();
